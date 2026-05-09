721ab20bfe87510f9004d48023ffdfcfb02d7f94
Nedie Yassin <yassinnedie9121@gmail.com>
2026-05-10 01:00:30 +0200

    Adding product to cart implementation

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,11 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setClick, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  setLocalStorage("so-cart", [product, ...cartItems]);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -13,6 +14,4 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+setClick("#addToCart", addToCartHandler);
